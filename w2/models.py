1d6e8d0888016aad26158476060d50c203a015dc
lin20011213 <lin20011213@gmail.com>
2023-10-04 20:11:57 +0800

    add:修正

--- w2/models.py
+++ w2/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
 class UserProfile(models.Model):
-    USER_UID = models.CharField(max_length=20)
-    Arm_UID = models.CharField(max_length=20)
-    Foot_UID = models.CharField(max_length=20)
-    Limb_UID = models.CharField(max_length=20)
-    Hand_UID = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    Arm_UID = models.CharField(max_length=20, unique=True)
+    Foot_UID = models.CharField(max_length=20, unique=True)
+    Limb_UID = models.CharField(max_length=20, unique=True)
+    Hand_UID = models.CharField(max_length=20, unique=True)
     TotalCoin = models.IntegerField(default=0)
 
-
-class ArmMetrics(models.Model):
-    Arm_UID = models.CharField(max_length=20)
-    USER_UID = models.CharField(max_length=20)
+class MetricsBase(models.Model):
+    USER_UID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     LastStage = models.CharField(max_length=20)
     TotalPlayTime = models.FloatField(default=0)
     TotalPlayCount = models.IntegerField(default=0)
@@ -23,42 +18,23 @@ class ArmMetrics(models.Model):
     TotalGetCoin = models.IntegerField(default=0)
     LastRecordId = models.CharField(max_length=20)
 
+    class Meta:
+        abstract = True
 
-class FootMetrics(models.Model):
-    Foot_UID = models.CharField(max_length=20)
-    USER_UID = models.CharField(max_length=20)
-    LastStage = models.CharField(max_length=20)
-    TotalPlayTime = models.FloatField(default=0)
-    TotalPlayCount = models.IntegerField(default=0)
-    PassCount = models.IntegerField(default=0)
-    TotalGetCoin = models.IntegerField(default=0)
-    LastRecordId = models.CharField(max_length=20)
+class ArmMetrics(MetricsBase):
+    Arm_UID = models.CharField(max_length=20, unique=True)
 
+class FootMetrics(MetricsBase):
+    Foot_UID = models.CharField(max_length=20, unique=True)
 
-class LimbMetrics(models.Model):
-    Limb_UID = models.CharField(max_length=20)
-    USER_UID = models.CharField(max_length=20)
-    LastStage = models.CharField(max_length=20)
-    TotalPlayTime = models.FloatField(default=0)
-    TotalPlayCount = models.IntegerField(default=0)
-    PassCount = models.IntegerField(default=0)
-    TotalGetCoin = models.IntegerField(default=0)
-    LastRecordId = models.CharField(max_length=20)
+class LimbMetrics(MetricsBase):
+    Limb_UID = models.CharField(max_length=20, unique=True)
 
-
-class HandMetrics(models.Model):
-    Hand_UID = models.CharField(max_length=20)
-    USER_UID = models.CharField(max_length=20)
-    LastStage = models.CharField(max_length=20)
-    TotalPlayTime = models.FloatField(default=0)
-    TotalPlayCount = models.IntegerField(default=0)
-    PassCount = models.IntegerField(default=0)
-    TotalGetCoin = models.IntegerField(default=0)
-    LastRecordId = models.CharField(max_length=20)
-
+class HandMetrics(MetricsBase):
+    Hand_UID = models.CharField(max_length=20, unique=True)
 
 class GameRecord(models.Model):
-    USER_UID = models.CharField(max_length=20)
+    USER_UID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     PlayDate = models.DateField()
     PlayPart = models.CharField(max_length=20)
     UID = models.IntegerField()
